78a2f577c15820635b875f50fc724514d6c46e35
a <a@pleasantonusd.net>
2017-11-29 09:29:42 -0800

    Hi

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ * 
+ * @author (Aashay Parab) 
+ * @version (08/17/2017)
+ */
+public class HelloWorld
+{
+    public static void main()
+{
+    System.out.println("Hello World");
+    System.out.println();
+    System.out.println("Aashay");
+    
+}
+
+    
+
+    
+    
+}
